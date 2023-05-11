ca82a8f28df49fbe082c25acb39438abd486784e
Nahir Veiras <nahir_vei@hotmail.com>
2023-05-11 17:10:00 -0300

    Modificaciones persona service

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -20,16 +20,16 @@ URL = 'https://nvbackend.onrender.com/personas/';
     return this.httpClient.get<persona>(this.URL + `detail/${id}`);
   }
 
-  /*public save(educacion: Educacion): Observable<any>{
-    return this.httpClient.post<any>(this.URL + 'create', educacion);
-  }*/
+  public save(Persona: persona): Observable<any>{
+    return this.httpClient.post<any>(this.URL + 'create', persona);
+  }
 
   public update(id: number, Persona: persona): Observable<any>{
     return this.httpClient.put<any>(this.URL + `update/${id}`, Persona);
   }
 
-  /*public delete(id: number): Observable<any>{
+  public delete(id: number): Observable<any>{
     return this.httpClient.delete<any>(this.URL + `delete/${id}`);
-  }*/
+  }
 }
 
